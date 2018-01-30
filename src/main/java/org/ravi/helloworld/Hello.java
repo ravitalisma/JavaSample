f4d2c4e08053910edf0be0d13df1243ed9dfcd4c
ravikk2 <ravi.kumar.k.s@accenture.com>
2018-01-30 19:02:09 +0530

    source file modified

--- src/main/java/org/ravi/helloworld/Hello.java
+++ src/main/java/org/ravi/helloworld/Hello.java
@@ -3,8 +3,12 @@ package org.ravi.helloworld;
 public class Hello {
 	
 	public String displayHelloMessage(String message){
-		message = "hi this changed";
-		return message;
+		String msg;
+		if(!Strings.isNullOrEmpty(String messages)) {
+			msg = messages;
+		}
+		else { msg = "hi"; }
+		return msg;
 	}
 
 }
